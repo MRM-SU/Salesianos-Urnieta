209856f88b77336d8416a681592ce0cb73194d8a
MRM-SU <rosco.markel@salesianosurnieta.com>
2025-10-09 10:39:20 +0200

    update 404

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',() => {
+    let firstScript = document.createElement('script');
+    firstScript.async = true;
+    firstScript.src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+    let secScript = document.createElement('script');
+    secScript.src="https://cdn.jsdelivr.net/npm/texme@1.2.2";
+})
